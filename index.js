e5efd9438186c1f88e9617f96400b53f4283c8a4
Jordan Needham <jdncreations@hotmail.com>
2023-07-23 23:09:26 +0800

    add active state, animate dice, fetch from api & update card

--- index.js
+++ index.js
@@ -1,3 +1,34 @@
+const button = document.querySelector('button');
+const id = document.getElementById('id');
+const advice = document.getElementById('advice');
+const dice = button.querySelector('svg');
+
 const API = "https://api.adviceslip.com/advice";
-console.log(API);
+
+button.classList.add('animateDice');
+getAdvice();
+
+
+// event listener button (dice)
+button.addEventListener('click', () => {
+
+  getAdvice();
+  button.classList.remove('animateDice');
+  void button.offsetWidth;
+  button.classList.add('animateDice');
+});
+
+// get advice from API
+async function getAdvice() {
+  // button.classList.add('animateDice');
+
+
+  const response = await fetch(API);
+  const slip = await response.json();
+  
+  
+  id.innerHTML = `advice # ${slip.slip.id}`;
+  advice.innerHTML = slip.slip.advice;
+}
+
 
